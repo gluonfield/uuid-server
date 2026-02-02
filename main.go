007f02b161f8ef9bffb5221c0259e5e477fe8f94
Augustinas Malinauskas <augustinasmal@gmail.com>
2026-02-01 23:28:13 -0800

    Initial commit: Go UUID server

---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/google/uuid"
+)
+
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		id := uuid.New()
+		w.Header().Set("Content-Type", "text/plain")
+		fmt.Fprintf(w, "%s\n", id.String())
+	})
+
+	port := "3000"
+	log.Printf("Server starting on port %s", port)
+	log.Fatal(http.ListenAndServe(":"+port, nil))
+}
